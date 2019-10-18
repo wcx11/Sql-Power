c9257438e5ce1a8e1a50031d5c1465ba00c60e06
wcx11 <wangchangxu11@gmail.com>
2019-10-18 21:31:08 +0800

    create react app

---
+++ src/sql/SqlConsumer.ts
@@ -0,0 +1,4 @@
+import * as antlr4 from 'antlr4';
+var TSqlLexer = require('./grammar/TSqlLexer').TSqlLexer;
+var TSqlParser = require('./grammar/TSqlParser').TSqlParser;
+var TSqlParserListener = require('./grammar/TSqlParserListener').TSqlParserListener;
