7a940274d2b49d6e88e651b5815cf1cf5ba5b36c
wcx11 <wangchangxu11@gmail.com>
2019-11-13 21:18:05 +0800

    start add things about m

---
+++ src/m/M.ts
@@ -0,0 +1,23 @@
+export class Table {
+
+}
+
+export class List {
+
+}
+
+export class Expression {
+
+}
+
+export class LetExpression {
+
+}
+
+export function SelectRowsFromTable() {
+
+}
+
+export function SelectColumnsFromTable() {
+
+}
